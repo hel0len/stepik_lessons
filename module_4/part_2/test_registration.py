c4602ed9d1b3cec3b70071e377aea4bfa7fe9bc9
helolen <hel0len53@gmail.com>
2020-12-06 12:34:04 +0300

    homework№4 part2 (fix)

--- module_4/part_2/test_registration.py
+++ module_4/part_2/test_registration.py
@@ -1,4 +1,5 @@
 import pytest
+import random
 from selenium import webdriver
 
 link = "http://selenium1py.pythonanywhere.com/ru/"
@@ -14,14 +15,12 @@ def browser():
 
 def test_registration_new_user(browser):
     #Data
+    unique_var = random.randint(10000, 9999999)
     button_login_or_registr_locator = '//a[@ id = "login_link"]'
     input_email_locator = '//input[@name="registration-email"]'
     input_password_locator = '//input[@ name = "registration-password1"]'
     input_confirm_password_locator = '//input[@ name = "registration-password2"]'
     button_register_locator = '//button[@ name = "registration_submit"]'
-    email = "random@gmail.com"
-    password = "2.fHhy!m48"
-    confirm_password = "2.fHhy!m48"
     success_registration_message_locator = '//*[contains(text(), "Спасибо за регистрацию!")]'
     success_registration_message = "Спасибо за регистрацию!"
 
@@ -31,11 +30,11 @@ def test_registration_new_user(browser):
     #Act
     browser.find_element_by_xpath(button_login_or_registr_locator).click()
     input_email = browser.find_element_by_xpath(input_email_locator)
-    input_email.send_keys(email)
+    input_email.send_keys("{}@mail.com".format(unique_var))
     input_password = browser.find_element_by_xpath(input_password_locator)
-    input_password.send_keys(password)
+    input_password.send_keys(unique_var, unique_var)
     input_confirm_password = browser.find_element_by_xpath(input_confirm_password_locator)
-    input_confirm_password.send_keys(confirm_password)
+    input_confirm_password.send_keys(unique_var, unique_var)
     browser.find_element_by_xpath(button_register_locator).click()
 
     #Assert
