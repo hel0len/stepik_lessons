e8acf018dd1a52d4d7a35091db99691207435caa
helolen <hel0len53@gmail.com>
2020-12-11 01:09:06 +0300

    first test (Page Object, assert)

--- module_5/test_main_page.py
+++ module_5/test_main_page.py
@@ -8,4 +8,9 @@ class TestMainPage:
     def test_guest_can_go_to_login_page(self, browser):
         page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
         page.open()
-        page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
+        page.go_to_login_page() # выполняем метод страницы - переходим на страницу логина
+
+    def test_guest_should_see_login_link(self, browser):
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
