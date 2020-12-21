92c659477e610d1a3e9facfabbb52e2b04f8703d
helolen <hel0len53@gmail.com>
2020-12-21 22:45:05 +0300

    hate OOP 2

--- module_5/pages/product_page.py
+++ module_5/pages/product_page.py
@@ -23,7 +23,7 @@ class ProductPage(BasePage):
         assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
            "Success message is presented, but should not be"
 
-    def is_dissappeared(self):
+    def is_disappeared(self):
         assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
-           "Success message is presented, but should not be"
+           "Success message is not disappeared, but should have"
 
--- module_5/test_product_page.py
+++ module_5/test_product_page.py
@@ -3,26 +3,45 @@ from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
 import pytest
 
-@pytest.mark.parametrize('promo', ["?promo=offer0",
-                                  "?promo=offer1",
-                                  "?promo=offer2",
-                                  "?promo=offer3",
-                                  "?promo=offer4",
-                                  "?promo=offer5",
-                                  "?promo=offer6",
-                                  "?promo=offer7",
-                                  "?promo=offer8",
-                                  "?promo=offer9"])
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 class TestProductPage():
-    def test_guest_can_add_product_to_basket(self, browser, promo):
-        link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/{promo}"
+    def test_guest_can_add_product_to_basket(self, browser):
         page = ProductPage(browser, link)
         page.open()
         page.add_to_basket()
-        page.solve_quiz_and_get_code()
+        #page.solve_quiz_and_get_code()
         page.should_be_book_name()
         page.should_be_book_price()
 
+    @pytest.mark.xfail(reason="wrong message")
+    def test_guest_cant_see_success_message_after_adding_product_to_basket(self, browser):
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_to_basket()
+        page.should_not_be_success_message()
 
+    def test_guest_cant_see_success_message(self, browser):
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+    @pytest.mark.xfail(reason="not disappiring")
+    def test_message_disappeared_after_adding_product_to_basket(self, browser):
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_to_basket()
+        page.is_disappeared()
+
+    def test_guest_should_see_login_link_on_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+
+    def test_guest_can_go_to_login_page_from_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.go_to_login_page()
